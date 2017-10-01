608f0b66110b5a09436d1917229bb0bff8da4510
Unknown <wieczorek.karolina1@o2.pl>
2017-10-01 23:50:55 +0200

    Http

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -6,29 +6,48 @@
 angular.module('blogDetail').
     component('blogDetail',{
         templateUrl:'/templates/blog-detail.html',
-        controller: function ($location, $routeParams, $scope) {
-            var blogItems = [
-                {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-09"},
-                {title: " Title", id: 2, description: "This is a book"},
-                {title: "Tea", id: 3, description: "This is a book"},
-                {title: "Lite", id: 4, description: "This is a book"},
-            ];
-            // console.log($routeParams)
-            $scope.title = "Blog " + $routeParams.id
-            $scope.notFound = true
+        controller: function ($http, $location, $routeParams, $scope) {
 
-            angular.forEach(blogItems, function (post) {
-                if (post.id == $routeParams.id){
-                    $scope.notFound = false
-                    $scope.post = post
-                }
+            $http.get("/json/posts.json", {}).then(successCallback, errorCallback);
 
-            });
+            function successCallback(response, status, config, statusText){
+                $scope.notFound = true
+
+                var blogItems = response.data
+                $scope.posts = blogItems
+
+                angular.forEach(blogItems, function(post) {
+                    if (post.id == $routeParams.id){
+                        $scope.notFound = true
+                        $scope.post = post
+                    }
+
+                });
+            }
+
+            function errorCallback(response, status, config, statusText){
+                console.log(response)
+            }
 
             if($scope.notFound){
                 console.log("Not Found")
                 //change location
                 $location.path("/404")
             }
+
+
+            // var blogItems = [
+            //     {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-09"},
+            //     {title: " Title", id: 2, description: "This is a book"},
+            //     {title: "Tea", id: 3, description: "This is a book"},
+            //     {title: "Lite", id: 4, description: "This is a book"},
+            // ];
+            // // console.log($routeParams)
+            // $scope.title = "Blog " + $routeParams.id
+            //
+            //
+
+
+
         }
     });
